ac69056c708e90a056b1037ec32049d3a0efcc7d
tunahua <leejunkilowinnive@gmail.com>
2022-04-15 01:36:22 +0800

    single page layout

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// mobile burger menu
+function openNav() {
+  document.getElementById("burger").style.height = "100%";
+}
+function closeNav() {
+  document.getElementById("burger").style.height = "0%";
+}
+
+// mobile share link fixed
