c2f1fa9d7bb7c333714c586ca3fd4f9426ec9836
tunahua <leejunkilowinnive@gmail.com>
2022-04-15 14:39:26 +0800

    修正部分區塊

--- js/index.js
+++ js/index.js
@@ -7,3 +7,31 @@ function closeNav() {
 }
 
 // mobile share link fixed
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  if (window.innerWidth <= 980) {
+    var shareLink = document.getElementById("shareLink");
+    var sticky = shareLink.clientHeight;
+    var viewportHeight = window.visualViewport.height;
+    var footerTop = document
+      .getElementById("innerFooter")
+      .getBoundingClientRect().top;
+    var articleBottom = innerArticle.getBoundingClientRect().bottom;
+    // console.log(viewportHeight);
+    // console.log(sticky);
+    // console.log(shareLink.getBoundingClientRect().top);
+    // console.log(footerTop);
+    // console.log(innerArticle.getBoundingClientRect().bottom + sticky);
+    // if (shareLink.getBoundingClientRect().top <= viewportHeight - sticky) {
+    // }
+    if (footerTop < viewportHeight || articleBottom + sticky > viewportHeight) {
+      shareLink.removeAttribute("style");
+    } else {
+      shareLink.style.position = "fixed";
+      shareLink.style.bottom = "0";
+    }
+  }
+}
